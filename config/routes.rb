c3de20cdf3ed4a8f2917b270c1d35e93378ec56e
Cess <cessmbuguar@gmail.com>
2017-11-05 23:05:56 +0300

    nested resourses for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :photos
-  resources :articles 
-  resources :events
+
+  resources :photos do
+    resouces :comments
+  end
+
+  resources :articles  do
+    resources :comments 
+  end
+
+  resources :events do
+    resources :comments 
+  end
+
   root  'articles#index'
 end
